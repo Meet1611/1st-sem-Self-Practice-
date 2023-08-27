3f75cca7dc3bcf48ab4dcc509dd8aa2534f8ab2f
Meet1611 <140803389+Meet1611@users.noreply.github.com>
2023-08-27 19:59:33 +0530

    Add files via upload

---
+++ Hollow inverted half pyramid.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main () {
+	int a,i,j;
+	cout << "Enter any number : ";
+	cin >> a;
+	cout << endl;
+	for (i=1 ; i<=a ; i++) {
+		for (j=1 ; j<=a ; j++) {
+			if (i==1 || j ==1 || (i+j)==(a+1)) {
+				cout << "*";
+			}
+			else {
+				cout << " ";
+			}
+	    }
+	    cout << endl;
+	}
+}

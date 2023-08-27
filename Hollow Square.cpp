d4423d9ec98c61a6c6659459c31e69aa534c5a28
Meet1611 <140803389+Meet1611@users.noreply.github.com>
2023-08-27 19:30:30 +0530

    Add files via upload

---
+++ Hollow Square.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main () {
+	int a,i,j;
+	cout << "Enter a number " << endl;
+	cin >> a;
+	for (i=1 ; i<=a ; i++) {
+		for (j=1 ; j<=a ; j ++) {
+			if (i==1 || j==1 || i==5 || j ==5) {
+				cout << "*";
+			}
+			else {
+				cout << " ";
+			}
+		}
+		cout << endl;
+	}
+}
